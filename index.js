9e11eb236c9f2d7bfb213cc5774c94895b5860cb
s3xys4s <s3xys4s@gmail.com>
2020-05-06 16:52:28 +0300

    ignoring config.json

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core-discord');
 
-const { token } = require('./config.json')
+const { token, prefix } = require('./config.json')
 
 const client = new Discord.Client();
-const prefix = 'cxc!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", msg => {
+client.on('message', msg => {
   if (!msg.content.startsWith(prefix)) return;
 });
 
 client.on('message', msg => {
   if (msg.content.startsWith(prefix + 'play')) {
-    console.log(msg.content)
+    console.log(msg.content.split(''))
     //msg.reply()
     //connection.play(await ytdl(url), { type: 'opus' });
   }
---
+++ readme.md
@@ -0,0 +1,28 @@
+## Simple music bot for discord
+
+#### 1. Install dependencies with a package manager of your choice
+```
+npm install
+# or 
+yarn install
+```
+
+#### 2. Create config.json
+```
+{
+  "token": "<bot_token>"
+}
+```
+
+Token can be obtained here: https://discord.com/developers/applications.
+
+### 3. Start it
+```
+npm start
+# or
+yarn start
+```
+
+### TODO:
+* pause/resume/ff/rewind/stop/exit commands
+* mb use redis for queue
